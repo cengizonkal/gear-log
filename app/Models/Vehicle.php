9ea4254e2d81876cec1277be0378a2d41cfb1bb7
cengiz <onkal.cengiz@gmail.com>
2025-04-11 08:35:42 +0300

    vehicle model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -5,8 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+*@property mixed $id
+*@property mixed $vehicle_model_id
+*@property mixed $license_plate
+*@property mixed $mileage
+*@property mixed $owner_id
+*@property mixed $fuel_type_id
+*@property mixed $vin
+*@property mixed $created_at
+*@property mixed $updated_at
+ */
 class Vehicle extends Model
 {
     /** @use HasFactory<\Database\Factories\VehicleFactory> */
     use HasFactory;
+
+    public function getRouteKeyName(): string
+    {
+        return 'license_plate';
+    }
+
+    public function vehicleModel()
+    {
+        return $this->belongsTo(VehicleModel::class);
+    }
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class);
+    }
+    public function fuelType()
+    {
+        return $this->belongsTo(FuelType::class);
+    }
+    
 }
